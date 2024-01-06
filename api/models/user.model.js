e7a11cbef78a8d91e09e6b107125956bafb9b1fb
devzil579 <122096643+devzil579@users.noreply.github.com>
2024-01-06 14:57:23 +0600

    Create sign up API route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const userSchema = new  mongoose.Schema({
-    userName:{
+    username:{
         type: String,
         required: true,
         unique: true,
@@ -17,6 +17,7 @@ const userSchema = new  mongoose.Schema({
         
     }
 },{timestamps:true});
- mongoose.model('User',userSchema)
 
-export default mongoose.model('User',userSchema)
+const User =  mongoose.model('User',userSchema)
+
+export default User
