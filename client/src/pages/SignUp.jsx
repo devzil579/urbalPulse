310649450f9ece200a8b54d6fcd87df5d5267a19
devzil579 <122096643+devzil579@users.noreply.github.com>
2024-01-07 12:02:09 +0600

    Complete sign up page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='border-2 gap-4 p-3 max-w-96 mx-auto'>
+      <h1 className='text-center font-bold text-xl'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input className='p-3 focus:outline-none' placeholder='email' type='email' value=''/>
+        <input className='p-3 focus:outline-none' placeholder='password' type='password' value=''/>
+        <button  className='bg-slate-700 text-white p-3 rounded-lg' type='submit'>Sign Up</button>
+      </form>
+      <p className='py-2' >Have an account <a className='text-blue-600'  href='/sign-in'>Sign in</a></p>
+    </div>
   )
 }
